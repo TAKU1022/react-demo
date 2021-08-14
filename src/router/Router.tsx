3834ec153aa6398a21dfa3811110bd30777a8757
Takumi <nase49946@gmail.com>
2021-08-14 15:51:47 +0900

    レイアウト反映

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,9 @@
-import { memo } from 'react';
-import { VFC } from 'react';
+import { memo, VFC } from 'react';
 import { Route, Switch } from 'react-router';
 
 import { Login } from '../components/pages/Login';
 import { Page404 } from '../components/pages/Page404';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
 import { HomeRoutes } from './HomeRoutes';
 
 export const Router: VFC = memo(() => {
@@ -22,7 +22,7 @@ export const Router: VFC = memo(() => {
                 exact={route.exact}
                 path={`${url}${route.path}`}
               >
-                {route.children}
+                <HeaderLayout>{route.children}</HeaderLayout>
               </Route>
             ))}
           </Switch>
