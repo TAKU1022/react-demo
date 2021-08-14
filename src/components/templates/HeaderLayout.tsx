62108bb18f065c686162813b92bf81f5313d99de
Takumi <nase49946@gmail.com>
2021-08-14 15:54:46 +0900

    ディレクトリ名変更

--- src/components/templates/HeaderLayout.tsx
+++ src/components/templates/HeaderLayout.tsx
@@ -1,6 +1,6 @@
 import { memo, ReactNode, VFC } from 'react';
 
-import { Header } from '../organisms/layouts/Header';
+import { Header } from '../organisms/layout/Header';
 
 type props = {
   children: ReactNode;
