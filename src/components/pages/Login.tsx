f7840b2c5dd8ddaad6affa45ce1bb0573f6e2fd3
Takumi <nase49946@gmail.com>
2021-08-14 18:27:03 +0900

    ログイン画面コンポーネント分割

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,13 +1,6 @@
 import { memo, VFC } from 'react';
-import {
-  Flex,
-  Box,
-  Heading,
-  Divider,
-  Input,
-  Button,
-  Stack,
-} from '@chakra-ui/react';
+import { Flex, Box, Heading, Divider, Input, Stack } from '@chakra-ui/react';
+import { PrimaryButton } from '../atoms/button/PrimaryButton';
 
 export const Login: VFC = memo(() => {
   return (
@@ -19,9 +12,7 @@ export const Login: VFC = memo(() => {
         <Divider my={4}></Divider>
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="ユーザーID"></Input>
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
