20252250f7c6297a98b7184785c0592930fc247c
Takumi <nase49946@gmail.com>
2021-08-15 17:31:19 +0900

    ユーザー一覧取得機能実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,36 @@
-import { memo, VFC } from 'react';
-import { Wrap, WrapItem } from '@chakra-ui/react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, VFC } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
 import { UserCard } from '../organisms/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
+import { User } from '../../types/user';
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="ああああああああ"
-          fullName="aaaaaaaa"
-        ></UserCard>
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner></Spinner>
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user: User) => {
+            return (
+              <WrapItem key={user.id} mx="auto">
+                <UserCard
+                  imageUrl="https://source.unsplash.com/random"
+                  userName={user.username}
+                  fullName={user.name}
+                ></UserCard>
+              </WrapItem>
+            );
+          })}
+        </Wrap>
+      )}
+    </>
   );
 });
