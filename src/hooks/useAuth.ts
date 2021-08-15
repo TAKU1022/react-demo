5374c7871eed30c59ccf2faa351037d13524c700
Takumi <nase49946@gmail.com>
2021-08-15 14:32:31 +0900

    メッセージ表示機能実装

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,28 +1,36 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useState } from 'react';
 import axios from 'axios';
 import { User } from '../types/user';
 import { useHistory } from 'react-router-dom';
+import { useMessage } from './useMessage';
 
 export const useAuth = () => {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
+  const { showMessage } = useMessage();
 
-  const login = useCallback((id: string) => {
-    setLoading(true);
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
 
-    axios
-      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        if (res.data) {
-          history.push('/home');
-        } else {
-          alert('ユーザーが見つかりません');
-        }
-      })
-      .catch(() => alert('ログインできません'))
-      .finally(() => setLoading(false));
-  }, []);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            showMessage({ title: 'ログインしました', status: 'success' });
+            history.push('/home');
+          } else {
+            showMessage({ title: 'ユーザーが見つかりません', status: 'error' });
+            alert('ユーザーが見つかりません');
+          }
+        })
+        .catch(() =>
+          showMessage({ title: 'ログインできません', status: 'error' })
+        )
+        .finally(() => setLoading(false));
+    },
+    [history, showMessage]
+  );
 
   return { login, loading };
 };
